702bdef1777790338c06e8bc6ff633a73b483fb3
Kaining xin <40644364+niuniu1994@users.noreply.github.com>
2020-04-23 09:17:22 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>{{ title }}</h1>
+    <button @click="iAmClicked()">Click me!</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  let num = 0;
+    export default {
+        data() {
+            return {title: 'Not clicked yet'}
+        },
+        methods: {
+            iAmClicked() {
+                this.onclick = num++;
+                this.title = 'Clicked '+ num +' times';
+            }
+        }
+    };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
