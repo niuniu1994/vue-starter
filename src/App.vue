9aa1fbcda24ff0b5d52317955de58336cfc2a12f
kaining <xkn1994@gmail.cin>
2020-04-30 09:48:21 +0200

    new form

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <template>
   <div>
-    <h1>{{ title }}</h1>
-    <button @click="iAmClicked()">Click me!</button>
+    <h1>Participants list ({{participants.length}})</h1>
+    <participants-list :list="participants"></participants-list>
+
+    <h3>New participant</h3>
+      <new-participant-form @added="addNewParticipant($event)"> </new-participant-form>
+    
   </div>
 </template>
 
 <script>
-  let num = 0;
-    export default {
-        data() {
-            return {title: 'Not clicked yet'}
-        },
-        methods: {
-            iAmClicked() {
-                this.onclick = num++;
-                this.title = 'Clicked '+ num +' times';
-            }
-        }
-    };
+    import NewParticipantForm from "./NewParticipantForm";
+    import ParticipantsList from "./ParticipantsList";
+  export default {
+    components:{NewParticipantForm,ParticipantsList},
+    data() {
+      return {
+        participants:[],
+      };
+    },
+    methods: {
+      addNewParticipant(newParticipant) {
+          this.participants.push(newParticipant);
+      }
+    }
+  };
 </script>
---
+++ src/NewParticipantForm.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <form @submit.prevent="addNewParticipant()">
+            <label>Firstname</label>
+            <input type="text" v-model="newParticipant.name">
+            <label>Lastname</label>
+            <input type="text" v-model="newParticipant.surname">
+            <button>Add new participant</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {newParticipant: {}};
+        },
+        methods: {
+            addNewParticipant() {
+                this.$emit('added', this.newParticipant);
+                this.newParticipant = {};
+            }
+        }
+    };
+</script>
